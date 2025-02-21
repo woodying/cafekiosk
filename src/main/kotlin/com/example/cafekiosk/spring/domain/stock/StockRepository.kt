ed86eb9a2271d488c9122210eebaef386ceb344b
woodying <wkyungjin1103@gmail.com>
2025-02-21 18:59:20 +0900

    refactor: 불필요한 Transactional annotation 제거

--- src/main/kotlin/com/example/cafekiosk/spring/domain/stock/StockRepository.kt
+++ src/main/kotlin/com/example/cafekiosk/spring/domain/stock/StockRepository.kt
@@ -1,6 +1,5 @@
 package com.example.cafekiosk.spring.domain.stock
 
-import jakarta.transaction.Transactional
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
@@ -9,7 +8,6 @@ interface StockRepository : JpaRepository<Stock, Long> {
     fun findByProductNumberIn(productNumber: Collection<String>): List<Stock>
 
     @Modifying(clearAutomatically = true)
-    @Transactional
     @Query(
         "UPDATE Stock s " +
                 "SET s.quantity = s.quantity + :inc " +
