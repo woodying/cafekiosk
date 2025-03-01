e386694037b47152575a8db43fe904eee5d19f3a
woodying <wkyungjin1103@gmail.com>
2025-03-01 22:33:10 +0900

    fix: ProductCreateRequest validation 오류

--- src/main/kotlin/com/example/cafekiosk/spring/api/controller/product/request/ProductCreateRequest.kt
+++ src/main/kotlin/com/example/cafekiosk/spring/api/controller/product/request/ProductCreateRequest.kt
@@ -1,12 +1,12 @@
 package com.example.cafekiosk.spring.api.controller.product.request
 
 import com.example.cafekiosk.spring.domain.product.Product
-import jakarta.validation.constraints.Size
+import jakarta.validation.constraints.Min
 
 data class ProductCreateRequest(
     val productType: Product.Type,
     val sellingStatus: Product.SellingStatus,
     val name: String,
-    @field:Size(min = 0, message = "price must be greater than or equal to 0")
+    @field:Min(value = 0, message = "price must be greater than or equal to 0")
     val price: Int
 )
