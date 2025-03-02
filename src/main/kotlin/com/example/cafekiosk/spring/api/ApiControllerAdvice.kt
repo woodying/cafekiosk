1126c1a6de818abf99d0a99680be192d413d225c
woodying <wkyungjin1103@gmail.com>
2025-03-02 21:29:29 +0900

    fix: ControllerAdvice -> RestControllerAdvice

--- src/main/kotlin/com/example/cafekiosk/spring/api/ApiControllerAdvice.kt
+++ src/main/kotlin/com/example/cafekiosk/spring/api/ApiControllerAdvice.kt
@@ -2,18 +2,20 @@ package com.example.cafekiosk.spring.api
 
 import org.springframework.http.HttpStatus
 import org.springframework.validation.BindException
-import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
 
-@ControllerAdvice
+@RestControllerAdvice
 class ApiControllerAdvice {
 
     @ExceptionHandler(BindException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun handleBindException(e: BindException): ApiResponse<*> {
         return ApiResponse(
             code = 400,
             status = HttpStatus.BAD_REQUEST,
-            message = e.allErrors.joinToString { it.defaultMessage ?: "" },
+            message = e.allErrors[0].defaultMessage,
             data = null,
         )
     }
