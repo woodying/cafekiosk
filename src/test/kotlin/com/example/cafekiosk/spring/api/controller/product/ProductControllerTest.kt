b5aa43e80ea1873787de05e4110566008e9dcae2
woodying <wkyungjin1103@gmail.com>
2025-03-02 21:30:23 +0900

    test: ProductController validation 테스트 추가

--- src/test/kotlin/com/example/cafekiosk/spring/api/controller/product/ProductControllerTest.kt
+++ src/test/kotlin/com/example/cafekiosk/spring/api/controller/product/ProductControllerTest.kt
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 
 @WebMvcTest(ProductController::class)
 class ProductControllerTest {
@@ -58,5 +59,42 @@ class ProductControllerTest {
         )
             .andDo(MockMvcResultHandlers.print())
             .andExpect(MockMvcResultMatchers.status().isOk)
+    }
+
+    @DisplayName("신규 상품을 생성할 때, 가격이 음수인 경우 예외를 발생시킨다.")
+    @Test
+    fun createProductWithMinusPrice() {
+        // given
+        val request = ProductCreateRequest(
+            productType = Product.Type.HANDMADE,
+            name = "아메리카노",
+            price = -1000,
+            sellingStatus = Product.SellingStatus.SELLING
+        )
+        every { productService.create(any()) } returns ProductResponse(
+            productNumber = "001",
+            type = request.productType,
+            sellingStatus = request.sellingStatus,
+            name = request.name,
+            price = request.price,
+            createdAt = null,
+            modifiedAt = null
+        )
+
+        // when, then
+        mockMvc.perform(
+            MockMvcRequestBuilders
+                .post("/api/v1/products/new")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+        )
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isBadRequest)
+            .andExpect(jsonPath("$.code").value("400"))
+            .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+            .andExpect(
+                jsonPath("$.message")
+                    .value("price must be greater than or equal to 0")
+            )
     }
 }
